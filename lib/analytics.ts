d3af5ca1d6f0fdb9eff00ee91876243df1b81b73
alaqmarrr <alaqmarak0810@hotmail.com>
2024-06-07 00:13:21 +0530

    updateAnalytics

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -2,17 +2,28 @@ import { get, getDatabase, ref, set } from "firebase/database";
 import { app } from "./firebase";
 
 const updateAnalytics = async () => {
-    const database = getDatabase(app);
-    const path = window.location.pathname
-    const analyticsRef = ref(database, `frame-the-vision/analytics/${path}`);
-    const snapshot = await get(analyticsRef);
-    if (snapshot.exists()) {
-        const data = snapshot.val();
-        set(analyticsRef, data + 1);
-    } else {
-        set(analyticsRef, 1);
-    }
+    try {
+        const database = getDatabase(app);
+        const path = window.location.pathname;
+        const isPathVisited = sessionStorage.getItem(path);
 
-}
+        if (isPathVisited) {
+            return;
+        } 
+
+        sessionStorage.setItem(path, 'true');
+        const analyticsRef = ref(database, `frame-the-vision/analytics${path}`);
+        const snapshot = await get(analyticsRef);
+
+        if (snapshot.exists()) {
+            const data = snapshot.val();
+            await set(analyticsRef, data + 1);
+        } else {
+            await set(analyticsRef, 1);
+        }
+    } catch (error) {
+        console.error("Failed to update analytics:", error);
+    }
+};
 
 export { updateAnalytics };
